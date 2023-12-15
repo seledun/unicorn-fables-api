8348cdf42cce9e1c4f4beec7d522545105d6f3aa
Sebastian Ledung <s.ledung@gmail.com>
2023-12-15 17:04:28 +0100

    Stubs for the API endpoints

--- README.md
+++ README.md
@@ -1,7 +1,10 @@
 Unicorn fables API
 
 ## Run Commands
-python -m venv venv
+### Körs endast första gången
+python -m venv ./venv
+
+### Körs för att starta programmet
 .venv/Scripts/activate
 pip install -r requirements.txt
 flask --app src/app run
--- src/app.py
+++ src/app.py
@@ -1,2 +1,46 @@
 import keys
+from flask import Flask
 
+API_VERSION = "0.0.1"
+
+app = Flask(__name__)
+
+# GET /version/unicorns
+@app.route("/" + API_VERSION + "/unicorns", methods=['GET'])
+def list_all_unicorns() :
+    return
+
+# POST /version/unicorns
+@app.route("/" + API_VERSION + "/unicorns", methods=['POST'])
+def submit_unicorn() :
+    return
+
+# POST /version/unicorns/<int:id>
+@app.route("/" + API_VERSION + "/unicorns/<int:id>", methods=['GET'])
+def get_unicorn() :
+    return
+
+# PUT /version/unicorns/<int:id>
+@app.route("/" + API_VERSION + "/unicorns/<int:id>", methods=['PUT'])
+def update_unicorn() :
+    return
+
+# GET /version/fables
+@app.route("/" + API_VERSION + "/fables", methods=['GET'])
+def list_all_fables() :
+    return
+
+# POST /version/fables
+@app.route("/" + API_VERSION + "/fables", methods=['POST'])
+def submit_fable() :
+    return
+
+# GET /version/fables/<int:id>
+@app.route("/" + API_VERSION + "/fables/<int:id>", methods=['GET'])
+def get_fable() :
+    return
+
+# PUT /version/fables/<int:id>
+@app.route("/" + API_VERSION + "/fables/<int:id>", methods=['PUT'])
+def update_fable() :
+    return
