022b075aba926d98e420a1e8e5ef33d107bcb484
M10P5306 <Jag_har_klippt_mig@hotmail.com>
2023-12-19 14:51:50 +0100

    Now we got unicorns!

--- src/app.py
+++ src/app.py
@@ -1,4 +1,8 @@
 import keys
+import requests
+import json
+import random
+from store.Unicorn import Unicorn
 from flask import Flask
 
 API_VERSION = "0.0.1"
@@ -44,3 +48,33 @@ def get_fable() :
 @app.route("/" + API_VERSION + "/fables/<int:id>", methods=['PUT'])
 def update_fable() :
     return
+
+
+
+def fetch_unicorns() -> list[Unicorn]:
+    
+    unicorns = []
+    response = requests.get("http://unicorns.idioti.se", headers={"Accept": "application/json"})
+
+    lenght = len(json.loads(response.text)) 
+
+    number_of_unicorns = []
+    for i in range(0, lenght):
+        number_of_unicorns.append(i)
+
+    count = 0
+    for i in range(0, lenght):
+
+        if count >= 5:
+            break
+
+        random_number = random.choice(number_of_unicorns)
+        number_of_unicorns.remove(random_number)
+        random_unicorn_id = response.json()[random_number].get("id")
+        unicorn = requests.get("http://unicorns.idioti.se/" + str(random_unicorn_id), headers={"Accept": "application/json"})
+        unicorns.append(unicorn.json())
+        count += 1
+
+    return unicorns
+    
+print(fetch_unicorns())
