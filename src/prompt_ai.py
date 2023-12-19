b36254be346afb0d6ebb29d4e8b370f4cdddb3c6
Emelie Eklund <109037739+EmelieEklund@users.noreply.github.com>
2023-12-19 14:28:09 +0100

    Updated prompt with variables

--- src/prompt_ai.py
+++ src/prompt_ai.py
@@ -4,16 +4,21 @@ import os
 # Load the API key from the .env file
 api_key = os.getenv("OPENAI_API_KEY")
 
+# Attach the API key directly to the OpenAI object / module
 OpenAI.api_key = api_key
 
 client = OpenAI()
+
+gender = "non-binary person"
+name = "Anna"
+unicorn_name = "Blobb"
 
 
 completion = client.chat.completions.create(
   model="gpt-3.5-turbo",   
   messages=[
-    {"role": "system", "content": "You are a sassy assistant, skilled in explaining unicorn mysticism with a creative flair."},
-    {"role": "user", "content": "Compose a fable including a unicorn called Unifaxsparkle and a child named Lou."}
+    {"role": "system", "content": "You are a magical assistant, skilled in telling whimsical and captivating unicorn fables with a mystic flair."},
+    {"role": "user", "content": "Compose a fable including a unicorn called " + unicorn_name + " and a " + gender + " named" + name + " ."}
   ]
 )
 
