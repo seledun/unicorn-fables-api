01f4d40a26f2f838b899de2525ee2a0c2ed6f022
M10P5306 <Jag_har_klippt_mig@hotmail.com>
2023-12-20 10:17:47 +0100

    metoder för enhörningar

--- src/app.py
+++ src/app.py
@@ -12,7 +12,11 @@ app = Flask(__name__)
 # GET /version/unicorns
 @app.route("/" + API_VERSION + "/unicorns", methods=['GET'])
 def list_all_unicorns() :
-    return
+    response = requests.get("http://unicorns.idioti.se", headers={"Accept": "application/json"})
+    modified_response = [] 
+    modified_response(json.dump(response.json().get("id"), response.json().get("name"))) 
+
+    return modified_response
 
 # POST /version/unicorns
 @app.route("/" + API_VERSION + "/unicorns", methods=['POST'])
@@ -21,13 +25,10 @@ def submit_unicorn() :
 
 # POST /version/unicorns/<int:id>
 @app.route("/" + API_VERSION + "/unicorns/<int:id>", methods=['GET'])
-def get_unicorn() :
-    return
-
-# PUT /version/unicorns/<int:id>
-@app.route("/" + API_VERSION + "/unicorns/<int:id>", methods=['PUT'])
-def update_unicorn() :
-    return
+def get_unicorn(id: int) :
+    
+    unicorn = requests.get("http://unicorns.idioti.se/" + str(id), headers={"Accept": "application/json"})
+    return unicorn
 
 # GET /version/fables
 @app.route("/" + API_VERSION + "/fables", methods=['GET'])
@@ -71,28 +72,83 @@ def fetch_unicorns() -> list[Unicorn]:
         random_number = random.choice(number_of_unicorns)
         number_of_unicorns.remove(random_number)
         random_unicorn_id = response.json()[random_number].get("id")
-        unicorn_response = requests.get("http://unicorns.idioti.se/" + str(random_unicorn_id), headers={"Accept": "application/json"})
+        unicorn_response = requests.get("http://unicorns.idioti.se/" + str(random_unicorn_id), headers={"Accept": "application/json"}) 
 
-
-        location = Location(unicorn_response.json().get("spottedWhere").get("name"), 
-                            unicorn_response.json().get("spottedWhere").get("lat"),
-                            unicorn_response.json().get("spottedWhere").get("lon"))
-
-        unicorn = Unicorn(unicorn_response.json().get("id"), 
-                          unicorn_response.json().get("image"), 
-                          unicorn_response.json().get("name"), 
-                          unicorn_response.json().get("spottedWhen"), 
-                          unicorn_response.json().get("description"), 
-                          unicorn_response.json().get("reportedBy"),
-                          location)
+        unicorn = build_a_unicorn(unicorn_response)
 
         unicorns.append(unicorn)
         count += 1
 
     return unicorns
+    
+
+def fetch_json_unicorns() -> list[Unicorn]:
+    
+    unicorns = []
+    response = requests.get("http://unicorns.idioti.se", headers={"Accept": "application/json"})
+
+    lenght = len(json.loads(response.text)) 
+
+    number_of_unicorns = []
+    for i in range(0, lenght):
+        number_of_unicorns.append(i)
+
+    count = 0
+    for i in range(0, lenght):
+
+        if count >= 5:
+            break
+
+        random_number = random.choice(number_of_unicorns)
+        number_of_unicorns.remove(random_number)
+        random_unicorn_id = response.json()[random_number].get("id")
+        unicorn = requests.get("http://unicorns.idioti.se/" + str(random_unicorn_id), headers={"Accept": "application/json"})
+        unicorns.append(unicorn.json())
+        count += 1
+
+    return unicorns
+
+def fetch_specific_unicorn_as_json(id: int) -> Unicorn:
+    
+    unicorn = requests.get("http://unicorns.idioti.se/" + str(id), headers={"Accept": "application/json"})
+    
+    return unicorn
+
+def fetch_specific_unicorn(id: int) -> Unicorn:
+
+    unicorn_response = requests.get("http://unicorns.idioti.se/" + str(id), headers={"Accept": "application/json"})
+
+    unicorn = build_a_unicorn(unicorn_response)
+    
+    return unicorn
+
+def build_a_unicorn(unicorn_parts: json) -> Unicorn:
+
+    location = Location(unicorn_parts.json().get("spottedWhere").get("name"), 
+                            unicorn_parts.json().get("spottedWhere").get("lat"),
+                            unicorn_parts.json().get("spottedWhere").get("lon"))
+
+    unicorn = Unicorn(unicorn_parts.json().get("id"), 
+                          unicorn_parts.json().get("image"), 
+                          unicorn_parts.json().get("name"), 
+                          unicorn_parts.json().get("spottedWhen"), 
+                          unicorn_parts.json().get("description"), 
+                          unicorn_parts.json().get("reportedBy"),
+                          location)
+    
+    return unicorn
+
+def list_of_unicorns() -> []:
+    response = requests.get("http://unicorns.idioti.se", headers={"Accept": "application/json"})
+    modified_response = [] 
 
 
-unicorns = fetch_unicorns()
+    lenght = len(json.loads(response.text))
+    for i in range(0, lenght):
+        unicorn_id = response.json()[i].get("id")
+        unicorn_name = response.json()[i].get("name")
+        modified_response.append(json.dumps({"id": unicorn_id, "name": unicorn_name}))
 
-for unicorn in unicorns:
-   print(unicorn.__str__())
+    return modified_response
+
+print(list_of_unicorns())
