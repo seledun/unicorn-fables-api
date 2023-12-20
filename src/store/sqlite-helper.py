61297c9b4ba888f2d54f1bc98ed010f687d21d3e
Sebastian Ledung <s.ledung@gmail.com>
2023-12-20 10:37:58 +0100

    Database setup

--- src/store/sqlite-helper.py
+++ src/store/sqlite-helper.py
@@ -6,8 +6,7 @@ import sqlite3
 def connect_to_database() :
     conn = sqlite3.connect('fables.db')
     conn.text_factory = str
-    c = conn.cursor()
-    return c
+    return conn
 
 # Save a fable to the database
 def save_fable_to_database(fable: Fable) :
@@ -17,15 +16,19 @@ def save_fable_to_database(fable: Fable) :
     fable_name = fable.name
     unicorn_id = fable.unicorn
 
-    c = connect_to_database()
-    c.execute('INSERT INTO fables (id, votes, text, name, unicorn) VALUES (?, ?, ?, ?, ?)', (fable_uuid, fable_votes, fable_text, fable_name, unicorn_id))
-    c.commit()
-    c.close()
+    conn = connect_to_database()
+    cursor = conn.cursor()
+    
+    cursor.execute('INSERT INTO fables (id, votes, text, name, unicorn) VALUES (?, ?, ?, ?, ?)', (fable_uuid, fable_votes, fable_text, fable_name, unicorn_id))
+    conn.commit()
+    cursor.close()
     return
 
 # Load fable from database and return fable object
 def load_fable_from_database(fable_id: int) -> Fable :
-    c = connect_to_database()
+    conn = connect_to_database()
+    c = conn.cursor()
+    
     c.execute('SELECT * FROM fables WHERE id = ?', (fable_id,))
     fable = c.fetchone()
     return fable
@@ -40,9 +43,10 @@ def save_unicorn_to_database(unicorn: Unicorn) :
     unicorn_place_lat = unicorn.place_lat
     unicorn_spotted_when = unicorn.spotted_when
 
-    c = connect_to_database()
+    conn = connect_to_database()
+    c = conn.cursor()
     c.execute('INSERT INTO unicorns (id, image, name, place_name, place_lon, place_lat, spotted_when) VALUES (?, ?, ?, ?, ?, ?, ?)', (unicorn_uuid, unicorn_image, unicorn_name, unicorn_place_name, unicorn_place_lon, unicorn_place_lat, unicorn_spotted_when))
-    c.commit()
+    conn.commit()
     c.close()
     return
 
